8cfe81442b3fba68cf1377be3de98f348b46f981
Kushal Nandwani <kushalnandwani98@gmail.com>
2022-01-04 15:11:28 +0530

    v1

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,81 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+// ------------------------------------------------------------------
+
+var user = prompt(`Enter your Name!`); 
+alert(`Welcome, ${user}`);
+
+var userName = user;
+console.log (`User : ${user} & UserName : ${userName}`);
+let age = 22, isLearning = true;
+
+user = "John"; 
+console.log (user, userName);
+
+// -----------------------------------------------------------------
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+// -------------------------------------------------------------------------------
+
+let numA = Number(prompt(`Enter any Number`));
+let numB = Number(prompt(`Enter any Number`));
+let sum = numA + numB;
+alert(`The sum will be ${sum}`);
+
+// ---------------------------------------------------------------------------
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+// ----------------------------------------------------------------------
+
+for (let j = 0; j <= 10; j++){
+    if(j % 2 == 0){
+        console.log(j);
+    }
+}
+
+// ----------------------------------------------------------------------------
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+// -------------------------------------------------------------------------
+
+for (let j = 9; j >= 0; j--) {
+    if(j % 2 !==0){
+        console.log(j)
+    }
+}
+
+// -------------------------------------------------------------------------
+
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+// --------------------------------------------------
+let j = 5;
+while(j >= 0) {
+    console.log (j);
+    (j--);
+}
+// --------------------------------------------------
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+// ------------------------------------------------------
+let j = 10;
+while (j >= 0) {
+    if (j % 2 == 0){
+        console.log (j);
+    }
+    (j--);
+}
+// ------------------------------------------------
 
 // Comparisoin
 
@@ -36,3 +96,16 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = prompt(`Enter a number!`);
+let num2 = prompt(`Enter a number!`);
+
+if (num1 == num2) {
+    alert(`true`);
+} 
+else if (num1 !== num2){
+    alert(`false`);
+}
+else {
+    alert(`Enter a valid value`);
+}
